755fb12a0026f55cff24f0955a7d7712a6b3c386
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2017-04-10 19:08:49 +0300

    Bump version to 1.3.7, update Forge & mappings and the version json

--- src/main/java/fi/dy/masa/justenoughdimensions/config/JustEnoughDimensionsGuiFactory.java
+++ src/main/java/fi/dy/masa/justenoughdimensions/config/JustEnoughDimensionsGuiFactory.java
@@ -1,6 +1,7 @@
 package fi.dy.masa.justenoughdimensions.config;
 
 import java.util.Set;
+import javax.annotation.Nullable;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraftforge.fml.client.IModGuiFactory;
@@ -19,6 +20,18 @@ public class JustEnoughDimensionsGuiFactory implements IModGuiFactory
     }
 
     @Override
+    public boolean hasConfigGui()
+    {
+        return true;
+    }
+
+    @Override
+    public GuiScreen createConfigGui(GuiScreen parentScreen)
+    {
+        return new JustEnoughDimensionsConfigGui(parentScreen);
+    }
+
+    @Override
     public Set<RuntimeOptionCategoryElement> runtimeGuiCategories()
     {
         return null;
@@ -26,6 +39,7 @@ public class JustEnoughDimensionsGuiFactory implements IModGuiFactory
 
     @SuppressWarnings("deprecation")
     @Override
+    @Nullable
     public RuntimeOptionGuiHandler getHandlerFor(RuntimeOptionCategoryElement element)
     {
         return null;
