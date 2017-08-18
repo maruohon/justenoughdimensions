b8b85a062ceabf594c0346d9dd6587f4667e3647
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2017-08-18 16:19:39 +0300

    Sign the JAR

--- src/main/java/fi/dy/masa/justenoughdimensions/reference/Reference.java
+++ src/main/java/fi/dy/masa/justenoughdimensions/reference/Reference.java
@@ -5,6 +5,7 @@ public class Reference
     public static final String MOD_ID = "justenoughdimensions";
     public static final String MOD_NAME = "Just Enough Dimensions";
     public static final String MOD_VERSION = "@MOD_VERSION@";
+    public static final String FINGERPRINT = "@FINGERPRINT@";
 
     public static final String PROXY_CLIENT = "fi.dy.masa.justenoughdimensions.proxy.ClientProxy";
     public static final String PROXY_SERVER = "fi.dy.masa.justenoughdimensions.proxy.CommonProxy";
