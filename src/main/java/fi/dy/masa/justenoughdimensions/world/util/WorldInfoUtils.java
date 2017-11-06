8a5271636870c7cc6aac4d1912a00b450c2c8a79
Matti Ruohonen <matti.j.ruohonen@gmail.com>
2017-11-06 12:47:03 +0200

    Change WorldInfo overriding for hopefully better Sponge-compatibility
    WorldInfo instances are now only overridden with WorldInfoJED, if the
    original one is the vanilla DerivedWorldInfo.
    Otherwise, assuming it's not already WorldInfoJED, only the properties
    defined in the dimensions.json config file are applied to the existing
    WorldInfo instance.

--- src/main/java/fi/dy/masa/justenoughdimensions/world/util/WorldInfoUtils.java
+++ src/main/java/fi/dy/masa/justenoughdimensions/world/util/WorldInfoUtils.java
@@ -8,9 +8,16 @@ import javax.annotation.Nullable;
 import net.minecraft.nbt.CompressedStreamTools;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.datafix.FixTypes;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.EnumDifficulty;
+import net.minecraft.world.GameType;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldSettings;
+import net.minecraft.world.WorldType;
+import net.minecraft.world.storage.DerivedWorldInfo;
 import net.minecraft.world.storage.SaveHandler;
 import net.minecraft.world.storage.WorldInfo;
+import net.minecraftforge.common.util.Constants;
 import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.relauncher.ReflectionHelper;
 import net.minecraftforge.fml.relauncher.ReflectionHelper.UnableToFindFieldException;
@@ -44,19 +51,33 @@ public class WorldInfoUtils
      */
     public static void loadAndSetCustomWorldInfo(World world)
     {
-        int dimension = world.provider.getDimension();
+        final int dimension = world.provider.getDimension();
 
-        // The WorldInfoJED check is necessary (or at least avoids setting things twice)
-        // for the case where WorldEvent.CreateSpawnPosition has ran before WorldEvent.LOAD
-        // and already set it, OR when using JED WorldProviders, when the
-        // WorldInfo override first happens from WorldProvider#setDimension() and
-        // then again from WorldEvent.Load.
-        if ((world.getWorldInfo() instanceof WorldInfoJED) == false &&
-            DimensionConfig.instance().useCustomWorldInfoFor(dimension))
+        if (DimensionConfig.instance().useCustomWorldInfoFor(dimension))
         {
-            JustEnoughDimensions.logInfo("WorldInfoUtils.loadAndSetCustomWorldInfo(): Overriding the existing " +
-                                         "WorldInfo with WorldInfoJED for dimension {}", dimension);
-            setWorldInfo(world, createCustomWorldInfoFor(world, dimension));
+            // The DerivedWorldInfo check is necessary for the case where WorldEvent.CreateSpawnPosition
+            // has ran before WorldEvent.Load and already set it, OR when using one of the JED WorldProviders,
+            // in which case the WorldInfo override first happens from WorldProvider#setDimension() and
+            // then would happen again from WorldEvent.Load.
+            // It's also necessary with Sponge, which uses their own WorldProperties stuff in place of the vanilla WorldInfo.
+            if (world.getWorldInfo().getClass() == DerivedWorldInfo.class)
+            {
+                JustEnoughDimensions.logInfo("WorldInfoUtils.loadAndSetCustomWorldInfo(): Overriding the existing " +
+                                             "DerivedWorldInfo with WorldInfoJED in dimension {}", dimension);
+
+                WorldInfoJED info = createCustomWorldInfoFor(world, dimension);
+                setWorldInfo(world, info);
+            }
+            // We might be running under Sponge here, so instead of overriding the WorldInfo instance, try to apply
+            // the configured properties to the existing one...
+            else if ((world.getWorldInfo() instanceof WorldInfoJED) == false)
+            {
+                JustEnoughDimensions.logInfo("WorldInfoUtils.loadAndSetCustomWorldInfo(): Applying WorldInfo properties " +
+                        "to an existing WorldInfo instance of type '{}' in dimension {}", world.getWorldInfo().getClass().getName(), dimension);
+
+                applyValuesToExistingWorldInfo(world, dimension);
+                applyChangesFromNewWorldInfo(world);
+            }
         }
     }
 
@@ -68,15 +89,38 @@ public class WorldInfoUtils
      */
     private static WorldInfoJED createCustomWorldInfoFor(World world, int dimension)
     {
-        NBTTagCompound nbt = loadWorldInfoFromFile(world, WorldFileUtils.getWorldDirectory(world));
-        NBTTagCompound playerNBT = world.getWorldInfo().getPlayerNBTTagCompound();
-        boolean isDimensionInit = nbt == null;
+        NBTTagCompound nbt = getWorldInfoTag(world, dimension, true, true);
+        return new WorldInfoJED(nbt);
+    }
 
-        // No level.dat exists for this dimension yet, inherit the values from the overworld
+    /**
+     * Returns the NBTTagCompound for creating a new WorldInfo instance for the given World.
+     * If both <i>readFromDisk</i> and <i>inheritFromOverworld</i> are false, then the returned tag
+     * will only contain the values defined in the "worldinfo" objects in the dimension config.
+     * @param world
+     * @param dimension
+     * @param readFromDisk
+     * @param inheritFromOverworld
+     * @return
+     */
+    private static NBTTagCompound getWorldInfoTag(World world, int dimension, boolean readFromDisk, boolean inheritFromOverworld)
+    {
+        NBTTagCompound nbt = readFromDisk ? loadWorldInfoFromFile(world, WorldFileUtils.getWorldDirectory(world)) : null;
+        final boolean isDimensionInit = readFromDisk ? nbt == null : levelFileExists(world) == false;
+
+        // No level.dat exists for this dimension yet
         if (nbt == null)
         {
-            // Get the values from the overworld WorldInfo
-            nbt = world.getWorldInfo().cloneNBTCompound(playerNBT);
+            if (inheritFromOverworld)
+            {
+                // Get the values from the overworld WorldInfo
+                NBTTagCompound playerNBT = world.getWorldInfo().getPlayerNBTTagCompound();
+                nbt = world.getWorldInfo().cloneNBTCompound(playerNBT);
+            }
+            else
+            {
+                nbt = new NBTTagCompound();
+            }
         }
 
         // Any tags/properties that are set in the dimensions.json take precedence over the level.dat
@@ -89,7 +133,7 @@ public class WorldInfoUtils
             DimensionConfig.instance().setWorldInfoValues(dimension, nbt, WorldInfoType.ONE_TIME);
         }
 
-        return new WorldInfoJED(nbt);
+        return nbt;
     }
 
     public static void saveCustomWorldInfoToFile(World world)
@@ -101,6 +145,19 @@ public class WorldInfoUtils
         {
             saveWorldInfoToFile(world, WorldFileUtils.getWorldDirectory(world));
         }
+    }
+
+    private static boolean levelFileExists(World world)
+    {
+        File worldDir = WorldFileUtils.getWorldDirectory(world);
+
+        if (worldDir != null)
+        {
+            File levelFile = new File(worldDir, "level.dat");
+            return levelFile.exists() && levelFile.isFile();
+        }
+
+        return false;
     }
 
     private static NBTTagCompound loadWorldInfoFromFile(World world, @Nullable File worldDir)
@@ -143,37 +200,41 @@ public class WorldInfoUtils
             try
             {
                 field_worldInfo.set(world, info);
-
-                if (world.provider instanceof IWorldProviderJED)
-                {
-                    // This sets the WorldType (the terrainType field) and the generatorSettings field
-                    // from the newly overridden WorldInfoJED
-                    world.provider.setWorld(world);
-                }
-
-                WorldBorderUtils.setWorldBorderValues(world);
-
-                // Override/re-create the ChunkProvider when using overridden WorldInfo,
-                // and NOT using one of the JED WorldProviders.
-                // Otherwise the ChunkProvider would be using the settings from the overworld, because
-                // WorldEvent.Load (where the WorldInfo gets overridden) only happens after
-                // the world has been constructed and the ChunkProvider set.
-
-                // However, for JED WorldProviders the WorldInfo, and more importantly,
-                // the terrainType and generatorSettings in the WorldProvider get set/overridden earlier
-                // than WorldEvent.Load, via the WorldProvider#setDimension() method, which gets called from
-                // the WorldServer constructor. Therefore the ChunkProvider for JED WorldProviders
-                // already has the correct settings available to it when it gets created in the first place.
-                if ((world.provider instanceof IWorldProviderJED) == false)
-                {
-                    WorldUtils.reCreateChunkProvider(world);
-                }
+                applyChangesFromNewWorldInfo(world);
             }
             catch (Exception e)
             {
                 JustEnoughDimensions.logger.error("WorldInfoUtils.setWorldInfo(): Failed to override WorldInfo for dimension {}",
                         world.provider.getDimension());
             }
+        }
+    }
+
+    private static void applyChangesFromNewWorldInfo(World world)
+    {
+        if (world.provider instanceof IWorldProviderJED)
+        {
+            // This sets the WorldType (the terrainType field) and the generatorSettings field
+            // from the newly overridden or updated WorldInfo
+            world.provider.setWorld(world);
+        }
+
+        WorldBorderUtils.setWorldBorderValues(world);
+
+        // Override/re-create the ChunkProvider when using overridden WorldInfo,
+        // and NOT using one of the JED WorldProviders.
+        // Otherwise the ChunkProvider would be using the settings from the overworld, because
+        // WorldEvent.Load (where the WorldInfo gets overridden) only happens after
+        // the world has been constructed and the ChunkProvider set.
+
+        // However, for JED WorldProviders the WorldInfo, and more importantly,
+        // the terrainType and generatorSettings in the WorldProvider get set/overridden earlier
+        // than WorldEvent.Load, via the WorldProvider#setDimension() method, which gets called from
+        // the WorldServer constructor. Therefore the ChunkProvider for JED WorldProviders
+        // already has the correct settings available to it when it gets created in the first place.
+        if ((world.provider instanceof IWorldProviderJED) == false)
+        {
+            WorldUtils.reCreateChunkProvider(world);
         }
     }
 
@@ -238,5 +299,99 @@ public class WorldInfoUtils
             JustEnoughDimensions.logger.error("WorldInfoUtils.saveWorldInfoToFile(): Failed to save world "+
                                               "info to file for dimension {}", world.provider.getDimension(), e);
         }
+    }
+
+    /**
+     * Apply the settings from the "worldinfo" objects in the dimension config to an existing WorldInfo instance.
+     * This is mainly meant for compatibility with Sponge or other mods that have and need their own versions of WorldInfo.
+     * @param world
+     * @param dimension
+     */
+    private static void applyValuesToExistingWorldInfo(World world, int dimension)
+    {
+        // Get just the values from the config
+        NBTTagCompound nbt = getWorldInfoTag(world, dimension, false, false);
+        WorldInfo info = world.getWorldInfo();
+
+        // These don't have setters, so they need to be smuggled in via WorldSettings
+        if (nbt.hasKey("RandomSeed", Constants.NBT.TAG_LONG) || nbt.hasKey("generatorOptions", Constants.NBT.TAG_STRING))
+        {
+            final long seed = nbt.hasKey("RandomSeed", Constants.NBT.TAG_LONG) ? nbt.getLong("RandomSeed") : info.getSeed();
+            GameType gameType = nbt.hasKey("GameType", Constants.NBT.TAG_INT) ? GameType.getByID(nbt.getInteger("GameType")) : info.getGameType();
+            boolean mapFeatures = nbt.hasKey("MapFeatures", Constants.NBT.TAG_BYTE) ? nbt.getBoolean("MapFeatures") : info.isMapFeaturesEnabled();
+            boolean hardcore = nbt.hasKey("hardcore", Constants.NBT.TAG_BYTE) ? nbt.getBoolean("hardcore") : info.isHardcoreModeEnabled();
+            WorldType worldType = nbt.hasKey("generatorName", Constants.NBT.TAG_STRING) ? getWorldType(nbt) : info.getTerrainType();
+
+            WorldSettings settings = new WorldSettings(seed, gameType, mapFeatures, hardcore, worldType);
+            settings.setGeneratorOptions(nbt.getString("generatorOptions"));
+
+            info.populateFromWorldSettings(settings);
+        }
+
+        if (nbt.hasKey("generatorName", Constants.NBT.TAG_STRING) && nbt.hasKey("generatorOptions", Constants.NBT.TAG_STRING) == false)
+        {
+            info.setTerrainType(getWorldType(nbt));
+        }
+
+        if (nbt.hasKey("GameType", Constants.NBT.TAG_INT))          { info.setGameType(GameType.getByID(nbt.getInteger("GameType"))); }
+        if (nbt.hasKey("MapFeatures", Constants.NBT.TAG_BYTE))      { info.setMapFeaturesEnabled(nbt.getBoolean("MapFeatures")); }
+        if (nbt.hasKey("Time", Constants.NBT.TAG_LONG))             { info.setWorldTotalTime(nbt.getLong("Time")); }
+        if (nbt.hasKey("DayTime", Constants.NBT.TAG_LONG))          { info.setWorldTime(nbt.getLong("DayTime")); }
+        if (nbt.hasKey("LevelName", Constants.NBT.TAG_STRING))      { info.setWorldName(nbt.getString("LevelName")); }
+        if (nbt.hasKey("clearWeatherTime", Constants.NBT.TAG_INT))  { info.setCleanWeatherTime(nbt.getInteger("clearWeatherTime")); }
+        if (nbt.hasKey("rainTime", Constants.NBT.TAG_INT))          { info.setRainTime(nbt.getInteger("rainTime")); }
+        if (nbt.hasKey("raining", Constants.NBT.TAG_BYTE))          { info.setRaining(nbt.getBoolean("raining")); }
+        if (nbt.hasKey("thunderTime", Constants.NBT.TAG_INT))       { info.setThunderTime(nbt.getInteger("tunherTime"));}
+        if (nbt.hasKey("thundering", Constants.NBT.TAG_BYTE))       { info.setThundering(nbt.getBoolean("tundering")); }
+        if (nbt.hasKey("hardcore", Constants.NBT.TAG_BYTE))         { info.setHardcore(nbt.getBoolean("hardcore")); }
+        if (nbt.hasKey("allowCommands", Constants.NBT.TAG_BYTE))    { info.setAllowCommands(nbt.getBoolean("allowCommands")); }
+        if (nbt.hasKey("Difficulty", Constants.NBT.TAG_BYTE))       { info.setDifficulty(EnumDifficulty.getDifficultyEnum(nbt.getByte("Difficulty"))); }
+        if (nbt.hasKey("DifficultyLocked", Constants.NBT.TAG_BYTE)) { info.setDifficultyLocked(nbt.getBoolean("DifficultyLocked")); }
+
+        if (nbt.hasKey("BorderCenterX", Constants.NBT.TAG_DOUBLE))          { info.getBorderCenterX(nbt.getDouble("BorderCenterX")); }
+        if (nbt.hasKey("BorderCenterZ", Constants.NBT.TAG_DOUBLE))          { info.getBorderCenterZ(nbt.getDouble("BorderCenterZ")); }
+        if (nbt.hasKey("BorderSize", Constants.NBT.TAG_DOUBLE))             { info.setBorderSize(nbt.getDouble("BorderSize")); }
+        if (nbt.hasKey("BorderSizeLerpTime", Constants.NBT.TAG_LONG))       { info.setBorderLerpTime(nbt.getLong("BorderSizeLerpTime")); }
+        if (nbt.hasKey("BorderSizeLerpTarget", Constants.NBT.TAG_DOUBLE))   { info.setBorderLerpTarget(nbt.getDouble("BorderSizeLerpTarget")); }
+        if (nbt.hasKey("BorderSafeZone", Constants.NBT.TAG_DOUBLE))         { info.setBorderSafeZone(nbt.getDouble("BorderSafeZone")); }
+        if (nbt.hasKey("BorderDamagePerBlock", Constants.NBT.TAG_DOUBLE))   { info.setBorderDamagePerBlock(nbt.getDouble("BorderDamagePerBlock")); }
+        if (nbt.hasKey("BorderWarningBlocks", Constants.NBT.TAG_INT))       { info.setBorderWarningDistance(nbt.getInteger("BorderWarningBlocks")); }
+        if (nbt.hasKey("BorderWarningTime", Constants.NBT.TAG_INT))         { info.setBorderWarningTime(nbt.getInteger("BorderWarningTime")); }
+
+        if (nbt.hasKey("SpawnX", Constants.NBT.TAG_INT) &&
+            nbt.hasKey("SpawnY", Constants.NBT.TAG_INT) &&
+            nbt.hasKey("SpawnZ", Constants.NBT.TAG_INT))
+        {
+            info.setSpawn(new BlockPos(nbt.getInteger("SpawnX"), nbt.getInteger("SpawnY"), nbt.getInteger("SpawnZ")));
+        }
+
+        if (nbt.hasKey("GameRules", Constants.NBT.TAG_COMPOUND))
+        {
+            info.getGameRulesInstance().readFromNBT(nbt.getCompoundTag("GameRules"));
+        }
+    }
+
+    private static WorldType getWorldType(NBTTagCompound nbt)
+    {
+        String name = nbt.getString("generatorName");
+        WorldType worldType = WorldType.parseWorldType(name);
+
+        if (worldType == null)
+        {
+            worldType = WorldType.DEFAULT;
+        }
+        else if (worldType.isVersioned())
+        {
+            int version = 0;
+
+            if (nbt.hasKey("generatorVersion", Constants.NBT.TAG_ANY_NUMERIC))
+            {
+                version = nbt.getInteger("generatorVersion");
+            }
+
+            worldType = worldType.getWorldTypeForGeneratorVersion(version);
+        }
+
+        return worldType;
     }
 }
