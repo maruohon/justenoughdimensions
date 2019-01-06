fe3318de49a414dad7d725aba9ce7ba3a1cc80c1
ferreusveritas <ferreusveritas@gmail.com>
2019-01-06 12:05:37 -0500

    Change whitespace for Masa

--- src/main/java/fi/dy/masa/justenoughdimensions/client/render/ShaderCheck.java
+++ src/main/java/fi/dy/masa/justenoughdimensions/client/render/ShaderCheck.java
@@ -5,19 +5,24 @@ import java.util.function.Function;
 import net.minecraftforge.fml.common.event.FMLInterModComms;
 
 public class ShaderCheck implements Function<Boolean, Void> {
-	private static boolean shaderOn = false;
-	
-	public static void init() {
-		FMLInterModComms.sendFunctionMessage("unifine", "shadertest", "fi.dy.masa.justenoughdimensions.client.render.ShaderCheck");
-	}
-	
-	@Override
-	public Void apply(Boolean shader) {
-		shaderOn = shader;
-		return null;
-	}
-	
-	public static boolean isShaderOn() {
-		return shaderOn;
-	}
+
+    private static boolean shaderOn = false;
+
+    public static void init()
+    {
+        FMLInterModComms.sendFunctionMessage("unifine", "shadertest", "fi.dy.masa.justenoughdimensions.client.render.ShaderCheck");
+    }
+
+    @Override
+    public Void apply(Boolean shader)
+    {
+        shaderOn = shader;
+        return null;
+    }
+
+    public static boolean isShaderOn()
+    {
+        return shaderOn;
+    }
+
 }
