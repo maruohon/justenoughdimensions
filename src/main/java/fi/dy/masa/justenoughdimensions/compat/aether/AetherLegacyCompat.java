37d3f86138d136d07cf77e9ad7082305706a2aa6
Petschko <peter@petschko.org>
2020-11-16 02:53:07 +0100

    Actually changed assignment to field, so the name makes more sense... (were was my head?)

--- src/main/java/fi/dy/masa/justenoughdimensions/compat/aether/AetherLegacyCompat.java
+++ src/main/java/fi/dy/masa/justenoughdimensions/compat/aether/AetherLegacyCompat.java
@@ -2,6 +2,7 @@ package fi.dy.masa.justenoughdimensions.compat.aether;
 
 import com.gildedgames.the_aether.api.AetherAPI;
 import com.gildedgames.the_aether.api.player.IPlayerAether;
+import com.gildedgames.the_aether.api.player.util.IAccessoryInventory;
 import fi.dy.masa.justenoughdimensions.util.PlayerInventoryHandler;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
@@ -20,29 +21,29 @@ public class AetherLegacyCompat {
 
     public static class InventoryViewAetherLegacy implements PlayerInventoryHandler.IInventoryView
     {
-        private final IPlayerAether inv;
+        private final IAccessoryInventory inv;
 
         public InventoryViewAetherLegacy(EntityPlayer player)
         {
-            this.inv = AetherAPI.getInstance().get(player);
+            this.inv = AetherAPI.getInstance().get(player).getAccessoryInventory();
         }
 
         @Override
         public int getSlotCount()
         {
-            return this.inv != null ? this.inv.getAccessoryInventory().getSizeInventory() : 0;
+            return this.inv != null ? this.inv.getSizeInventory() : 0;
         }
 
         @Override
         public ItemStack getStack(int slot)
         {
-            return this.inv.getAccessoryInventory().getStackInSlot(slot);
+            return this.inv.getStackInSlot(slot);
         }
 
         @Override
         public void setStack(int slot, ItemStack stack)
         {
-            this.inv.getAccessoryInventory().setInventorySlotContents(slot, stack);
+            this.inv.setInventorySlotContents(slot, stack);
         }
     }
 }
